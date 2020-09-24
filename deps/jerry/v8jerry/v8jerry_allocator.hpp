c6018799b315b7327289f7509497fb20049865bf
Robert Fancsik <frobert@inf.u-szeged.hu>
2020-09-24 15:08:55 +0200

    Add missing vptr table to JerryAllocator
    Signed-off-by: Robert Fancsik <frobert@inf.u-szeged.hu>

--- deps/jerry/v8jerry/v8jerry_allocator.hpp
+++ deps/jerry/v8jerry/v8jerry_allocator.hpp
@@ -4,16 +4,14 @@
 #include <jerryscript.h>
 #include <v8.h>
 
-class JerryAllocator;
-
-class JerryAllocator {
+class JerryAllocator : public v8::ArrayBuffer::Allocator {
 public:
     JerryAllocator() = default;
 
-    void* Allocate(size_t length);
-    void* AllocateUninitialized(size_t length);
-    void Free(void* data, size_t length);
-    void* Reallocate(void* data, size_t old_length, size_t new_length);
+    virtual void* Allocate(size_t length);
+    virtual void* AllocateUninitialized(size_t length);
+    virtual void Free(void* data, size_t length);
+    virtual void* Reallocate(void* data, size_t old_length, size_t new_length);
 
     static JerryAllocator* NewDefaultAllocator();
     static v8::ArrayBuffer::Allocator* toV8(JerryAllocator* alloc) { return reinterpret_cast<v8::ArrayBuffer::Allocator*>(alloc); }
