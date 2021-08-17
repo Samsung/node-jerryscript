515a2438d45716833b0d2de5ec7af2b6fd44deaa
Gergo Csizi <csgergo92@gmail.com>
2021-08-17 14:36:48 +0200

    Integrate jerry SharedArrayBuffer
    JerryScript-DCO-1.0-Signed-off-by: Gergo Csizi csgergo92@gmail.com

--- deps/jerry/v8jerry/v8jerry_atomics.cpp
+++ deps/jerry/v8jerry/v8jerry_atomics.cpp
@@ -3,23 +3,6 @@
 
 namespace JerryAtomics {
 
-/* SharedArrayBuffer */
-static jerry_value_t shared_array_buffer_constructor(const jerry_call_info_t *call_info_p,
-                                                     const jerry_value_t args_p[],
-                                                     const jerry_length_t args_cnt)
-{
-    if ((args_cnt == 0) || (jerry_value_is_number(args_p[0]) == false)) {
-        return jerry_create_undefined();
-    }
-
-    jerry_length_t size = (jerry_length_t) jerry_get_number_value(args_p[0]);
-    jerry_value_t array = jerry_create_arraybuffer(size);
-
-    // TODO: Create mutex for memory sharing
-
-    return array;
-}
-
 /* Atomics */
 static jerry_value_t atomics_handler(const jerry_call_info_t *call_info_p,
                                      const jerry_value_t args_p[],
@@ -81,14 +64,7 @@ static jerry_value_t atomics_load_handler(const jerry_call_info_t *call_info_p,
     return jerry_get_property_by_index(args_p[0], index);
 }
 
-void InitializeSharedArrayBuffer() {
-    JerryxHandlerRegisterGlobal((const jerry_char_t*) "SharedArrayBuffer",
-                                    shared_array_buffer_constructor);
-    // TODO: Create map of mutexes for memory sharing
-}
-
 void Initialize() {
-    InitializeSharedArrayBuffer();
     JerryxHandlerRegisterGlobal((const jerry_char_t*)"Atomics",
                                     atomics_handler);
     jerry_value_t global_obj_val = jerry_get_global_object();
